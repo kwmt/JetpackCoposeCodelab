b7f933c559411a3a5a49cf646c7ef17f2a934d03
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-10-27 19:14:41 +0900

    状態の更新

--- app/src/main/java/net/kwmt27/jetpackcoposecodelab/MainActivity.kt
+++ app/src/main/java/net/kwmt27/jetpackcoposecodelab/MainActivity.kt
@@ -3,6 +3,7 @@ package net.kwmt27.jetpackcoposecodelab
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.Model
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.core.setContent
@@ -37,12 +38,13 @@ fun MyApp(child: @Composable() () -> Unit) {
 }
 
 @Composable
-fun MyScreenContent() {
+fun MyScreenContent(appState: AppState = AppState()) {
     Column {
         Greeting(name = "Android")
         Divider(color = Color.Black)
         Greeting(name = "there")
-
+        Divider(color = Color.Transparent, height = 32.dp)
+        Counter(appState.counterState)
     }
 }
 
@@ -50,6 +52,20 @@ fun MyScreenContent() {
 fun Greeting(name: String) {
     Text(text = "Hello $name!", modifier = Spacing(24.dp))
 }
+
+@Model
+class CounterState(var count: Int = 0)
+
+class AppState(val counterState: CounterState = CounterState())
+
+@Composable
+fun Counter(state: CounterState) {
+    Button(
+        text = "I've been clicked ${state.count} times",
+        onClick = {
+            state.count++
+        }
+    )
 
 val myNames = listOf("Manuel", "Nick", "Jose", "Florina", "Yacine")
 
