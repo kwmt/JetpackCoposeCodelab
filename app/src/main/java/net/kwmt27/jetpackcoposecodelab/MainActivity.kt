ab185cfb382a21a96e22bdcf2774fcc19f8e41a6
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-10-27 18:03:56 +0900

    リファクタリング

--- app/src/main/java/net/kwmt27/jetpackcoposecodelab/MainActivity.kt
+++ app/src/main/java/net/kwmt27/jetpackcoposecodelab/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
 import androidx.ui.graphics.Color
+import androidx.ui.layout.Spacing
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.surface.Surface
 import androidx.ui.tooling.preview.Preview
@@ -14,18 +16,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Greeting("Android")
-            }
+            MyApp()
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    MaterialTheme() {
+        Surface(color = Color.Yellow) {
+            Greeting(name = "Android")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = Color.Yellow) {
-        Text(text = "Hello $name!")
-    }
+    Text(text = "Hello $name!", modifier = Spacing(24.dp))
 }
 
 @Preview
